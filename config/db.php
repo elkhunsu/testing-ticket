83355fa4c8910e2384d16c3013e293ce38edfbf0
elkhunsu <elkin.khunsu@gmail.com>
2018-05-11 16:24:59 +0700

    make it better

--- config/db.php
+++ config/db.php
@@ -5,6 +5,5 @@ return [
     'dsn' => 'mysql:host=ou6zjjcqbi307lip.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=vzqpxowgl6yblvuk',
     'username' => 'kawzwhwa61a3ln4f',
     'password' => 'odhtr6grplid7neq',
-    'charset' => 'utf8',
-    'port' => '3306'
-];
+    'charset' => 'utf8'
+    ];
